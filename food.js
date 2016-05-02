184b5caaba25f162ef6eba4f993d2a64764612a9
Michael Mead <michaelmead007@icloud.com>
2016-05-02 13:38:33 -0500

    finish json DOM input

--- food.js
+++ food.js
@@ -14,7 +14,14 @@ function printFood(data){
   for (var i = 0; i<data.length; i++) {
     var v = data[i];
     console.log("v brands", v);
-    dogFood.innerHTML += v.name;
+    dogFood.innerHTML += '<p class = "name">' + v.name + '</p>';// + '<p>' + v.types[i].type + '</P>' + '<p>' + v.types[i].volumes[i].name + '</p>' + '<p>' + v.types[i].volumes[i].price + '</p>';
+    for (var j=0; j < v.types.length; j++) {
+      dogFood.innerHTML += '<p class = "type">' + v.types[j].type + '</P>';
+      for (var k=0; k < v.types.length; k++) {
+        dogFood.innerHTML += '<p class = "volume">' + v.types[j].volumes[k].name + '</P>' +  '<p class = "price">' + v.types[j].volumes[k].price + '</P>';
+      }
+    }
+  }
 }
-}
+
 
