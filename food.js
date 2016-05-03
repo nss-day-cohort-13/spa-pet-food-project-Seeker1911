3ceced309d5f28b151e27a97db37fe773b9a02ed
Michael Mead <michaelmead007@icloud.com>
2016-05-03 10:29:40 -0500

    start new json file

--- food.js
+++ food.js
@@ -11,17 +11,20 @@ function loadFood() {
 }
 
 function printFood(data){
+  var matter = "";
   for (var i = 0; i<data.length; i++) {
     var v = data[i];
     console.log("v brands", v);
-    dogFood.innerHTML += '<p class = "name">' + v.name + '</p>';// + '<p>' + v.types[i].type + '</P>' + '<p>' + v.types[i].volumes[i].name + '</p>' + '<p>' + v.types[i].volumes[i].price + '</p>';
+    matter  +=  '<div class = card>' + '<p class = "name">' + v.name + '</p>';// + '<p>' + v.types[i].type + '</P>' + '<p>' + v.types[i].volumes[i].name + '</p>' + '<p>' + v.types[i].volumes[i].price + '</p>';
     for (var j=0; j < v.types.length; j++) {
-      dogFood.innerHTML += '<p class = "type">' + v.types[j].type + '</P>';
+      matter += '<p class = "type">' + v.types[j].type + '</P>';
       for (var k=0; k < v.types.length; k++) {
-        dogFood.innerHTML += '<p class = "volume">' + v.types[j].volumes[k].name + '</P>' +  '<p class = "price">' + v.types[j].volumes[k].price + '</P>';
+      matter += '<p class = "volume">' + v.types[j].volumes[k].name + '</P>' +  '<p class = "price">' + v.types[j].volumes[k].price + '</P>';
       }
     }
+    matter += '</div>';
   }
-}
 
+ dogFood.innerHTML = matter;
+}
 
